55b64137b465b163d1e147a7f7b9abeac23e91e8
muhammadfuaad <fuaad29@gmail.com>
2023-01-08 08:28:53 +0500

    project setup complete, Happy Coding

---
+++ src/components/assistance.js
@@ -0,0 +1,9 @@
+function Assistance() {
+  return (
+    <div className="assistance flex">
+      <p><span>Need further assistance? </span><span>We’re here to help</span></p>
+      <button className="btn btn--primary btn--large">Contact Us</button>
+    </div>
+  );
+}
+export default Assistance;
