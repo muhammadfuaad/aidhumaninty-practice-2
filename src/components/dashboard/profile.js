3de9a29ef9b3b38b1d4fa21aeef2807cd6e7d481
muhammadfuaad <fuaad29@gmail.com>
2023-01-12 20:38:34 +0500

    some issues in radio and input field

--- src/components/dashboard/profile.js
+++ src/components/dashboard/profile.js
@@ -12,31 +12,25 @@ function Profile() {
             <p className="text-[1.8rem] font-bold tracking-[-0.45px] text-headings mb-8">Info</p>
             <div className="flex flex-col space-y-8">
               <form className="flex space-x-16">
-              <div class="flex items-center space-x-4">
+                <div class="flex items-center space-x-4">
                   <input id="mr" type="radio" value="" name="status" class="w-4 h-4 text-[#7c7c7c] bg-gray-100 border-gray-300 focus:ring-[#7c7c7c] focus:ring-2"></input>
                   <label for="mr" class="ml-2 text-[1.4rem] font-medium tracking-[-0.21px] text-[#181818]">Mr</label>
-              </div>
-
-
-                <div className="flex space-x-6">
-                  <input type="radio" id="html" name="fav_language" value="HTML"></input>
-                  <label for="html">Mr</label>
                 </div>
-                <div className="flex space-x-6">
-                  <input type="radio" id="html" name="fav_language" value="HTML"></input>
-                  <label for="html">Mr</label>
+                <div class="flex items-center space-x-4">
+                  <input id="mrs" type="radio" value="" name="status" class="w-4 h-4 text-[#7c7c7c] bg-gray-100 border-gray-300 focus:ring-[#7c7c7c] focus:ring-2"></input>
+                  <label for="mrs" class="ml-2 text-[1.4rem] font-medium tracking-[-0.21px] text-[#181818]">Mrs</label>
                 </div>
-                <div className="flex space-x-6">
-                  <input type="radio" id="html" name="fav_language" value="HTML"></input>
-                  <label for="html">Mr</label>
+                <div class="flex items-center space-x-4">
+                  <input id="miss" type="radio" value="" name="status" class="w-4 h-4 text-[#7c7c7c] bg-gray-100 border-gray-300 focus:ring-[#7c7c7c] focus:ring-2"></input>
+                  <label for="miss" class="ml-2 text-[1.4rem] font-medium tracking-[-0.21px] text-[#181818]">Miss</label>
                 </div>
-                <div className="flex space-x-6">
-                  <input type="radio" id="html" name="fav_language" value="HTML"></input>
-                  <label for="html">Mr</label>
+                <div class="flex items-center space-x-4">
+                  <input id="ms" type="radio" value="" name="status" class="w-4 h-4 text-[#7c7c7c] bg-gray-100 border-gray-300 focus:ring-[#7c7c7c] focus:ring-2"></input>
+                  <label for="ms" class="ml-2 text-[1.4rem] font-medium tracking-[-0.21px] text-[#181818]">Ms</label>
                 </div>
-                <div className="flex space-x-6">
-                  <input type="radio" id="html" name="fav_language" value="HTML"></input>
-                  <label for="html">Mr</label>
+                <div class="flex items-center space-x-4">
+                  <input id="other" type="radio" value="" name="status" class="w-4 h-4 text-[#7c7c7c] bg-gray-100 border-gray-300 focus:ring-[#7c7c7c] focus:ring-2"></input>
+                  <label for="other" class="ml-2 text-[1.4rem] font-medium tracking-[-0.21px] text-[#181818]">Other</label>
                 </div>
               </form>
               <div className="flex space-x-4">
@@ -57,6 +51,33 @@ function Profile() {
               <input type="text" id="email" className="block border-light rounded-xl px-2.5 pb-2.5 pt-5 w-full text-[1.4rem] text-gray-900 bg-gray-50 dark:bg-gray-700 peer" placeholder=" " />
               <label for="email" className="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-4 z-10 origin-[0] left-2.5 peer-focus:text-[#7c7c7c] peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4">Floating filled</label>
             </div>
+          </div>
+
+          <div className="p-8 border-bottom-medium">
+            <p className="portal-subheading mb-8">Address</p>
+            
+            
+            <button id="dropdownDefaultButton" data-dropdown-toggle="dropdown" class="text-black border-light font-medium rounded-lg text-sm px-4 py-2.5 text-center inline-flex items-center" type="button">Dropdown button <svg class="w-4 h-4 ml-2" aria-hidden="true" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path></svg></button>
+            
+            <div id="dropdown" class="z-10 hidden bg-white divide-y divide-gray-100 rounded shadow w-44 dark:bg-gray-700">
+                <ul class="py-1 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownDefaultButton">
+                  <li>
+                    <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Dashboard</a>
+                  </li>
+                  <li>
+                    <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Settings</a>
+                  </li>
+                  <li>
+                    <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Earnings</a>
+                  </li>
+                  <li>
+                    <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Sign out</a>
+                  </li>
+                </ul>
+            </div>
+
+
+
           </div>
         </div>
 
