5c16f254a7140b3c833e10dbdb2a1c7281fd3d4d
muhammadfuaad <fuaad29@gmail.com>
2023-01-23 12:36:42 +0500

    code

--- src/components/dashboard/profile_sidebar.js
+++ src/components/dashboard/profile_sidebar.js
@@ -3,25 +3,22 @@ import Funding_stats from "./funding_stats";
 import Zakat from "./zakat";
 import React from "react";
 import Image_upload from "./image_upload";
+import Profile_header from "./profile_header";
+import Profile_header_mobile from "./profile_header_mobile";
 
 function Profile_sidebar() {
   const [visibility, setVisibility] = React.useState(false);
   return (
-    <section className=" relative">
+    <section className="overflow-hidden relative">
       {visibility? <div className="fixed right-0 z-10"><Image_upload /></div> : null}
-      <img src="./Icons/footer-background-logo.svg" className="absolute left-40 top-[35rem] -z-10"></img>
-      <div className="flex justify-between items-center px-8 h-28 border-bottom-light">
-        <button className="flex space-x-4 items-center bg-primary-dark rounded-2xl py-4 px-6 h-fit">
-          <img src="./Icons/blue-plus.svg" className="w-4"></img>
-          <span className="text-white text-[1.4rem] font-semibold uppercase">New Appeal</span>
-        </button>
-        <div className="flex justify-between space-x-10 relative">
-          <div className="h-9 w-[1px] bg-[#bdbdbd] absolute left-[6.5rem] top-1"></div>
-          <a href="#" className="flex space-x-2 items-center"><span className="text-[1.4rem] font-medium tracking-[-0.21px] text-[#999999]">En</span><img src="./Icons/drop-down.svg" alt=""></img></a>
-          <a href="#" className="flex space-x-2 items-center"><img src="./Icons/logout.svg" className="w-8" alt=""></img><span className="text-[1.3rem] font-medium tracking-[-0.2px] text-headings">Log Out</span></a>
-        </div>
+      <img src="./Icons/footer-background-logo.svg" className="absolute left-40 top-[35rem] z-0 sm:-z-10"></img>
+      <div className="hidden sm:block">
+        <Profile_header />
       </div>
-      <div className="flex flex-col justify-center items-center px-8 pb-[30rem]">
+      <div className="sm:hidden">
+        <Profile_header_mobile />
+      </div>
+      <div className="flex flex-col justify-center items-center bg-[#fafafa] sm:bg-white px-8 pb-6 sm:pb-[30rem]">
         <div className="mt-20 w-48 h-48 rounded-[50%] bg-[#bdbdbd] flex justify-center items-center relative">
           <img src="./Icons/user-circle.svg" className="w-24 brightness-[1000]"></img>
           <a href="#" className="flex justify-center bg-[#e6e6e6] w-14 h-14 rounded-full absolute bottom-1 right-1 align-middle" onClick={()=>setVisibility(current=> !current)}><img src="./Icons/edit.svg" className="w-8"></img></a>
@@ -33,13 +30,13 @@ function Profile_sidebar() {
         </div>
         <button className="px-16 py-4 uppercase text-[1.4rem] font-semibold text-primary outline outline-2 outline-primary rounded-lg mt-8">View Profile</button>
         <button className="px-8 py-4 uppercase text-[1.4rem] font-semibold text-[#bdbdbd] outline outline-2 outline-[#bdbdbd] rounded-lg mt-6">Edit Profile</button>
-        <div className="mt-12 flex space-x-4">
+        <div className="mt-12 flex space-x-4 z-10">
         <Funding_stats title="Total raised" amount="£100" percentage="10%" classNames="bg-primary" symbol="plus"
           img="bottom" />
           <Funding_stats title="Total given" amount="£100" percentage="10%" classNames="bg-green" symbol="minus"
           img="top" />
         </div>
-        <div className="mt-12 w-full">
+        <div className="mt-12 w-full z-10">
           <Zakat />
         </div>
       </div>
