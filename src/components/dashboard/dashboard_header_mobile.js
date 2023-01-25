06a15c6c04faa0ea58bb4d5fdb88d7b351e9dcee
muhammadfuaad <fuaad29@gmail.com>
2023-01-25 18:06:58 +0500

    dashboard_header_mobile added

--- src/components/dashboard/dashboard_header_mobile.js
+++ src/components/dashboard/dashboard_header_mobile.js
@@ -1,14 +1,14 @@
 function Dashboard_header_mobile() {
   return (
-    <div className="flex justify-between">
+    <div className="flex justify-between bg-white p-6">
       <div className="flex space-x-4">
         <img src="./Icons/dashboard-sidebar-toggler.svg"></img>
         <img src="./Icons/dashboard-sidebar-logo.svg"></img>
       </div>
 
       <div className="flex space-x-4">
-        <img src="./Icons/plus-circle.svg"></img>
-        <img src="./Icons/user-circle.svg"></img>
+        <img src="./Icons/plus-circle.svg" className="w-8"></img>
+        <img src="./Icons/user-circle.svg" className="w-8"></img>
       </div>
     </div>
   );
