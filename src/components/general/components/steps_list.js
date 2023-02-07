83b09a947189116fa3af2323026c98a6466f7c54
muhammadfuaad <fuaad29@gmail.com>
2023-02-07 22:11:47 +0500

    code

--- src/components/general/components/steps_list.js
+++ src/components/general/components/steps_list.js
@@ -3,48 +3,50 @@ import Icon_2 from "../images/refine-message.svg"
 import Icon_3 from "../images/choose-amount.svg"
 import Icon_4 from "../images/text-link.svg"
 import Icon_5 from "../images/take-link.svg"
+import Icon_6 from "../images/black-check-green-circle.svg";
 function Steps_list() {
   return (
     <div>
       <div className="flex gap-20 items-center">
         <div className="flex gap-2 items-center">
-          <div className="flex justify-center items-center h-16 w-32 rounded-full bg-[#e6e6e6]"><p className="text-[1.8rem] font-semibold tracking-[-0.45px] text-black">1.</p></div>
-          <img src={Icon_1}></img>
+          <div className="flex justify-center items-center h-16 w-16 rounded-full bg-[#e6e6e6]"><p className="text-[1.8rem] font-semibold tracking-[-0.45px] text-black">1.</p></div>
+          <div className="h-64 w-64"><img src={Icon_1}></img></div>
         </div>
-        <p className="text-[1.6rem] font-normal tracking-[-0.4px] text-[#999] whitespace-normal pr-[30rem] mt-8">Choose an appeal based on a cause that means something to you (If you’d like Aid Humanity to back a cause you have an interest in please message us).</p>
+        <p className="text-[1.6rem] font-normal tracking-[-0.4px] text-[#999] whitespace-normal w-[40%] mt-8">Choose an appeal based on a cause that means something to you (If you’d like Aid Humanity to back a cause you have an interest in please message us).</p>
       </div>
 
       <div className="flex gap-20 items-center">
         <div className="flex gap-2 items-center">
-          <div className="flex justify-center items-center h-16 w-32 rounded-full bg-[#e6e6e6]"><p className="text-[1.8rem] font-semibold tracking-[-0.45px] text-black">2.</p></div>
+          <div className="flex justify-center items-center h-16 w-16 rounded-full bg-[#e6e6e6]"><p className="text-[1.8rem] font-semibold tracking-[-0.45px] text-black">2.</p></div>
           <img src={Icon_2}></img>
         </div>
-        <p className="text-[1.6rem] font-normal tracking-[-0.4px] text-[#999] whitespace-normal pr-[30rem] mt-8">Refine your message so everyone will instantly connect with it.</p>
+        <p className="text-[1.6rem] font-normal tracking-[-0.4px] text-[#999] whitespace-normal w-[40%] mt-8">Refine your message so everyone will instantly connect with it.</p>
       </div>
 
       <div className="flex gap-20 items-center">
         <div className="flex gap-2 items-center">
-          <div className="flex justify-center items-center h-16 w-32 rounded-full bg-[#e6e6e6]"><p className="text-[1.8rem] font-semibold tracking-[-0.45px] text-black">3.</p></div>
+          <div className="flex justify-center items-center h-16 w-16 rounded-full bg-[#e6e6e6]"><p className="text-[1.8rem] font-semibold tracking-[-0.45px] text-black">3.</p></div>
           <img src={Icon_3}></img>
         </div>
-        <p className="text-[1.6rem] font-normal tracking-[-0.4px] text-[#999] whitespace-normal pr-[30rem] mt-8">Choose the amount you want to raise and share it with the world.</p>
+        <p className="text-[1.6rem] font-normal tracking-[-0.4px] text-[#999] whitespace-normal w-[40%] mt-8">Choose the amount you want to raise and share it with the world.</p>
       </div>
       
       <div className="flex gap-20 items-center">
         <div className="flex gap-2 items-center">
-          <div className="flex justify-center items-center h-16 w-32 rounded-full bg-[#e6e6e6]"><p className="text-[1.8rem] font-semibold tracking-[-0.45px] text-black">4.</p></div>
+          <div className="flex justify-center items-center h-16 w-16 rounded-full bg-[#e6e6e6]"><p className="text-[1.8rem] font-semibold tracking-[-0.45px] text-black">4.</p></div>
           <img src={Icon_4}></img>
         </div>
-        <p className="text-[1.6rem] font-normal tracking-[-0.4px] text-[#999] whitespace-normal pr-[30rem] mt-8">Text the link to all your friends and family and ask them to share.</p>
+        <p className="text-[1.6rem] font-normal tracking-[-0.4px] text-[#999] whitespace-normal w-[40%] mt-8">Text the link to all your friends and family and ask them to share.</p>
       </div>
 
       <div className="flex gap-20 items-center">
         <div className="flex gap-2 items-center">
-          <div className="flex justify-center items-center h-16 w-32 rounded-full bg-[#e6e6e6]"><p className="text-[1.8rem] font-semibold tracking-[-0.45px] text-black">5.</p></div>
+          <div className="flex justify-center items-center h-16 w-16 rounded-full bg-[#e6e6e6]"><p className="text-[1.8rem] font-semibold tracking-[-0.45px] text-black">5.</p></div>
           <img src={Icon_5}></img>
         </div>
-        <p className="text-[1.6rem] font-normal tracking-[-0.4px] text-[#999] whitespace-normal pr-[30rem] mt-8">Take your link live on social media and build unstoppable momentum.</p>
+        <p className="text-[1.6rem] font-normal tracking-[-0.4px] text-[#999] whitespace-normal w-[40%] mt-8">Take your link live on social media and build unstoppable momentum.</p>
       </div>
+      <img src={Icon_6}></img>
     </div>
 
     
--- src/components/general/pages/marketing_page.js
+++ src/components/general/pages/marketing_page.js
@@ -13,14 +13,17 @@ function Marketing_page() {
         <div className="px-64">
           <p className="text-[1.6rem] font-normal tracking-[-0.24px] text-[#999] my-16">Home / <span className="text-body">Help Center</span> / Donation Policy</p>
           <div className="flex relative">
-            <div className="w-[80%] flex flex-col space-y-8 pb-[100px]">
+            <div className="w-[80%] flex flex-col pb-[100px]">
               <div className="flex flex-col space-y-8">
-                <div>
                   <p className="text-[3.6rem] font-bold tracking-[-0.54px] text-black mb-12">Marketing</p>
                   <p className="text-[3rem] font-bold tracking-[-0.75px] text-black mb-8">Marketing your campaign</p>
                   <p className="text-[2.2rem] font-normal tracking-[-0.55px] text-body leading-[3rem]">We want your campaign to be a sucess from the moment you launch it, which is why we’ve created a simple, straightforward process that covers everything. All you have to do is follow it step by step and watch as the world helps you change things for the better:</p>
                   <Steps_list />
-                </div>
+                  <p className="text-[2.2rem] font-normal tracking-[-0.55px] text-body">These quick steps will get your campaign up and running before you know it. From there, it’s over to you to innovate and create so that your ideas never run out of steam. We know you can do this, and the more passion you have, the more you’ll raise.</p>
+                    <button className="self-center px-20 h-20 py-6 uppercase text-[1.4rem] font-semibold text-white bg-primary rounded-xl">
+                      Start Fundraising Now
+                    </button>
+                    <p className="self-center text-[1.8rem] font-bold tracking-[-0.45px] text-black">Why wait a moment longer?</p>
               </div>
             </div>
             <div className="relative ml-32">
