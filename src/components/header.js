589571db7b7e3e867a0e42d19faaa57aceb21142
muhammadfuaad <fuaad29@gmail.com>
2023-02-11 10:22:14 +0500

    code

--- src/components/header.js
+++ src/components/header.js
@@ -9,23 +9,23 @@ function Header() {
   return (
     <div className="">
       <div className="flex justify-between text-[1.4rem] font-medium tracking-[-0.21px] text-[#999] py-4 px-64 border-b border-[#e6e6e6]">
-        <div className="flex gap-2">
+        <a href="#" className="flex gap-2">
           <p>En</p>
           <img src={Dropdown} className="w-4"></img>
-        </div>
+        </a>
 
         <div className="flex gap-4">
-          <p>Latest News</p>
-          <p>Zakat Calculator</p>
+          <a href="#">Latest News</a>
+          <a href="#">Zakat Calculator</a>
         </div>
 
         <div className="flex gap-16">
-          <div className="flex gap-4 relative">
+          <a href="#" className="flex gap-4 relative">
             <div className="bg-[#e6e6e6] h-6 w-[1px] absolute -right-8"></div>
             <img src={Phone}></img>
             <p className="font-bold text-primary">0330 057 9957</p>
-          </div>
-          <p>Contact Us</p>
+          </a>
+          <a href="#">Contact Us</a>
         </div>
       </div>
 
@@ -34,20 +34,24 @@ function Header() {
           <img src={Logo}></img>
           <ul className="flex justify-between gap-24 items-center text-[1.8rem] font-semibold tracking-[-0.27px] text-black
               ">
-            <li>Our Story</li>
-            <li>Appeals</li>
-            <li>Emergency</li>
-            <li>Zakat</li>
-            <li>Get Involved</li>
+            <li><a href="#">Our Story</a></li>
+            <li><a href="#">Appeals</a></li>
+            <li><a href="#">Emergency</a></li>
+            <li><a href="#">Zakat</a></li>
+            <li><a href="#">Get Involved</a></li>
           </ul>
         </div>
 
-        <div className="flex gap-8">
-          <div className="flex gap-4 items-center">
-            <img src={User} className="w-8"></img>
-            <p className="text-[1.4rem] font-semibold tracking-[-0.21px] text-black">My Account</p>
-          </div>
-          <img src={Heart} className="w-8"></img>
+        <div className="flex gap-8 items-center">
+          <a href="#">
+            <div className="flex gap-4 items-center mr-4">
+              <img src={User} className="w-8"></img>
+              <p className="text-[1.4rem] font-semibold tracking-[-0.21px] text-black whitespace-nowrap">My Account</p>
+            </div>
+          </a>
+          <a href="#">
+            <div><img src={Heart} className="w-8 h-12"></img></div>
+          </a>
           <button class="w-full py-6 uppercase text-[1.4rem] font-semibold text-primary bg-transparent outline
             outline-primary rounded-xl">Donate now</button>
         </div>
