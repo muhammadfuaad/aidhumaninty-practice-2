9730f428e4a43883e8979d04d947676ce65f8482
muhammadfuaad <fuaad29@gmail.com>
2023-02-14 20:51:46 +0500

    code

--- src/components/homepage/home_page.js
+++ src/components/homepage/home_page.js
@@ -1,13 +1,6 @@
 import Toggle from "../dashboard/toggle";
 import Generic_header from "../generic_header";
 import Footer from "../footer";
-import Radio_component from "../generic/components/radio_component";
-import Appeal_title from "../generic/components/appeal_title";
-import Mosque from "../icons/mosque.svg";
-import Water_drop from "../icons/water-drop.svg";
-import Hunger_appeal from "../icons/hunger-appeal.svg";
-import Emergency from "../icons/emergency.svg";
-import Sponsor_orphan from "../icons/sponsor-orphan.svg";
 import Appeal_card from "../generic/components/appeal_card";
 import Appeals_list from "../generic/components/appeals_list";
 import Logo from "../icons/logo-without-text.svg"
@@ -19,6 +12,8 @@ import donation_1 from "../icons/donation-1.png"
 import donation_2 from "../icons/donation-2.png"
 import donation_3 from "../icons/donation-3.png"
 import orange from "../icons/orange-1.png"
+import News from "./components/news";
+import Ways from "./components/ways";
 
 
 
@@ -53,55 +48,7 @@ function Home_page() {
         <p className="flex basis-[50%] items-center justify-center text-primary-dark text-[24px] font-semibold tracking-[-0.6px] leading-[28px]">Give Back - Deliver Better - Drive Change</p>
       </section>
 
-      <section>
-        <div className="flex flex-col space-y-8 bg-Ghost-white px-48 py-12">
-          <p className="text-black text-3xl font-bold tracking-[-0.75px]">Latest News</p>
-          <div className="flex">
-            <div className="basis-[50%] p-14 bg-cover bg-center">
-              <div className="w-[80px] h-[40px] bg-red text-center text-white font-semibold mt-40 px-2 py-3">
-                <p className="my-auto">NEW</p>
-              </div>
-              <p className="text-white text-[36px] font-bold tracking-[-0.54px]">Donec turpis eros, euismod nec justo sit amet</p>
-              <p className="text-F5 text-[1.6rem] font-normal tracking-[-0.4px]">Pellentesque consequat dui turpis, nec porta nisi varius quis.
-              Ut mattis velit quis mi consectetur, non rhoncus metus dapibus.</p>
-              <div className="flex space-x-2 items-center">
-                <p className="text-Testimonial-Gray text-[11px] font-normal tracking-[-0.17px]">July 3, 2022</p>
-                <p>.</p>
-                <p className="text-Testimonial-Gray text-[11px] font-normal tracking-[-0.17px]">5 minutes to read</p>
-              </div>
-            </div>
-
-            <div className="basis-[25%] flex-col">
-              <div className="px-8 space-y-1">
-                <p className="text-black text-lg font-bold tracking-[-0.45px]">Mauris vel ornare massa, at ullamcorper ligula</p>
-                <p className="text-Gray text-[1.6rem] font-normal tracking-[-0.4px]">Cras ullamcorper dolor ac viverra finibus. Fusce iaculis accumsan ex, in placerat arcu
-                luctus vitae. Fusce velit lacus, hendrerit scelerisque efficitur eget, placerat eu lectus.</p>
-                <div className="flex space-x-2 items-center">
-                  <p className="text-Testimonial-Gray text-[11px] font-normal tracking-[-0.17px]">April 20, 2022</p>
-                  <p>.</p>
-                  <p className="text-Testimonial-Gray text-[11px] font-normal tracking-[-0.17px]">3 minutes to read</p>
-                </div>
-              </div>
-            </div>
-
-            <div className="basis-[25%] flex-col">
-              <div className="px-8 space-y-1">
-                <p className="text-black text-lg font-bold tracking-[-0.45px]">Aenean ac iaculis urna, quis condimentum elit</p>
-                <p className="text-Gray text-[1.6rem] font-normal tracking-[-0.4px]">Nullam eleifend faucibus mi, ac dapibus lectus interdum eu. Suspendisse sed semper augue,
-                nec pulvinar orci. Praesent tincidunt purus condimentum efficitur fermentum.</p>
-                <div className="flex space-x-2 items-center">
-                  <p className="text-Testimonial-Gray text-[11px] font-normal tracking-[-0.17px]">March 7, 2022</p>
-                  <p>.</p>
-                  <p className="text-Testimonial-Gray text-[11px] font-normal tracking-[-0.17px]">4 minutes to read</p>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="flex justify-center items-center">
-            <button className="w-[450px] text-primary-dark font-medium rounded-lg border-Gray border-2 border-opacity-50 py-2">View All</button>
-          </div>
-        </div>
-      </section>
+      <News/>
       
       <section>
         <div className="flex items-center h-[150px] space-y-2 bg-primary px-48">
@@ -145,52 +92,7 @@ function Home_page() {
         </div>
       </section>
 
-      <section>
-        <div className="flex space-x-8 py-8">
-          <div className="basis-[60%] space-y-8 flex flex-col pl-48 bg-white">
-            <div className="flex flex-col mr-96 space-y-4">
-              <p className="text-black text-3xl font-bold tracking-[-0.75px]">Ways to give</p>
-              <p className="text-body text-[22px] font-normal tracking-[-0.55px]">Crowd funding lets people play a more important role in their charity.
-              Start building your network of giving today.</p>
-            </div>
-            <div className="flex flex-col space-y-4 pr-44">
-              <div className="flex items-center p-4 space-x-8  shadow-md border-Gray border-2 border-opacity-25 rounded-lg">
-                <p className="text-primary-dark text-[38px] font-medium tracking-[-0.95px]">01.</p>
-                <img src={donation_1} className="w-[9rem] h-[7rem]"></img>
-                <div className="flex flex-col space-y-2">
-                  <p className="text-black text-lg font-bold tracking-[-0.45px]">You could make a quick one off donation</p>
-                  <p className="text-Spanish-Gray text-[1.6rem] font-normal tracking-[-0.4px]">For those last minute donors or when your unsure of how much to give</p>
-                </div>
-              </div>
-              <div className="flex items-center p-4 space-x-8  shadow-md border-Gray border-2 border-opacity-25 rounded-lg">
-                <p className="text-primary-dark text-[38px] font-medium tracking-[-0.95px]">02.</p>
-                <img src={donation_2} className="w-[10rem] h-[10rem]"></img>
-                <div className="flex flex-col space-y-2">
-                  <p className="text-black text-lg font-bold tracking-[-0.45px]">Subscription donation</p>
-                  <p className="text-Spanish-Gray text-[1.6rem] font-normal tracking-[-0.4px]">Making a donation on every Friday or come Ramadan for that continuous support</p>
-                </div>
-              </div>
-              <div className="flex items-center p-4 space-x-8  shadow-md border-Gray border-2 border-opacity-25 rounded-lg">
-                <p className="text-primary-dark text-[38px] font-medium tracking-[-0.95px]">03.</p>
-                <img src={donation_3} className="w-[8.5rem] h-[10.5rem]"></img>
-                <div className="flex flex-col space-y-2">
-                  <p className="text-black text-lg font-bold tracking-[-0.45px]">Project Backed donation</p>
-                  <p className="text-Spanish-Gray text-[1.6rem] font-normal tracking-[-0.4px]">This is great when you back a project that’s close to your heart and receive updates</p>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div className="basis-[40%] flex flex-col">
-            <img src={orange}></img>
-            <div className="flex flex-col space-y-4 pr-48">
-              <p className="text-primary text-[36px] font-bold tracking-[-0.54px]">Don’t wait for the world to improve.</p>
-              <p className="text-body text-[22px] font-normal tracking-[-0.55px]">Drive meaningful change.</p>
-              <button className="w-[170px] h-[50px] text-primary border-2 border-primary rounded-lg">REGISTER NOW</button>
-            </div>
-          </div>
-        </div>
-      </section>
+      <Ways/>
 
 
       <div className="flex flex-col space-y-2 bg-amber px-48 py-12">
