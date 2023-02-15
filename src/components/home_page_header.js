22d54707154c6246fc4d66ac38a3d9ad99be7367
muhammadfuaad <fuaad29@gmail.com>
2023-02-15 11:39:05 +0500

    code

--- src/components/home_page_header.js
+++ src/components/home_page_header.js
@@ -10,44 +10,33 @@ import Login from './menu_options/pages/login';
 function Home_page_header() {
   return (
     <div className="">
-      <div className="flex justify-between text-[1.4rem] font-medium tracking-[-0.21px] text-[#999] py-4 px-64 border-b border-platinum">
-        <a href="#" className="flex gap-2">
-          <p>En</p>
-          <img src={Dropdown} className="w-4"></img>
-        </a>
-
-        <div className="flex gap-4">
-          <a href="#">Latest News</a>
-          <a href="#">Zakat Calculator</a>
-        </div>
-
-        <div className="flex gap-16">
-          <a href="#" className="flex gap-4 relative">
-            <div className="bg-platinum h-6 w-[1px] absolute -right-8"></div>
-            <img src={Phone}></img>
-            <p className="font-bold text-primary">0330 057 9957</p>
-          </a>
-          <a href="#">Contact Us</a>
-        </div>
-      </div>
-
-      <div className="flex justify-between px-64">
+      <div className="flex justify-between px-64 py-8">
         <div className="flex gap-24">
-          <img src={Logo}></img>
+          <img src={Logo} className="w-16"></img>
           <ul className="flex justify-between gap-24 items-center text-[1.8rem] font-semibold tracking-[-0.27px] text-black
-              ">
-            <li><a href="#">Our Story</a></li>
+              relative">
+            <div className="bg-platinum h-16 w-[1px] absolute -left-12"></div>
+            <li><a href="#" className="whitespace-nowrap">Our Story</a></li>
             <li className="group">
               <a href="#">Appeals</a>
               <div className="hidden group-hover:block"><Appeals_options/></div>
             </li>
-            <li><a href="#">Emergency</a></li>
-            <li><a href="#">Zakat</a></li>
-            <li><a href="#">Get Involved</a></li>
+            <li><a href="#" className="whitespace-nowrap">Emergency</a></li>
+            <li><a href="#" className="whitespace-nowrap">Zakat</a></li>
+            <li><a href="#" className="whitespace-nowrap">Get Involved</a></li>
           </ul>
         </div>
+        
 
         <div className="flex gap-8 items-center">
+          <div className="flex gap-16 items-center text-[1.4rem] font-medium tracking-[-0.21px] text-spanish-gray">
+            <a href="#" className="whitespace-nowrap">Zakat Calculator</a>      
+            <a href="#" className="flex gap-2 mr-4 relative">
+              <div className="bg-platinum h-8 w-[1px] absolute -left-8"></div>
+              <p>En</p>
+              <img src={Dropdown} className="w-4"></img>
+            </a>
+          </div>
           <a href="#">
             <div className="flex gap-4 items-center mr-4 group relative">
               <div className="hidden group-hover:block absolute top-8 left-[-25rem] z-10"><Login/></div>
@@ -56,7 +45,13 @@ function Home_page_header() {
             </div>
           </a>
           <a href="#">
-            <div><img src={Heart} className="w-8 h-12"></img></div>
+            <div className="relative">
+              <img src={Heart} className="w-20"></img>
+              <div className="bg-primary rounded-full w-6 h-6 flex justify-center items-center absolute -bottom-2 -right-2">
+                <p className="text-[1rem] font-bold text-white">1</p>
+              </div>
+            </div>
+            
           </a>
           <button className="w-full py-6 uppercase text-[1.4rem] font-semibold text-primary bg-transparent outline
             outline-primary rounded-xl">Donate now</button>
