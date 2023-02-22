c247aa1b3acd3efcd473877d3aeff66460ce351c
muhammadfuaad <fuaad29@gmail.com>
2023-02-22 17:38:32 +0500

    code

--- src/components/homepage/components/contact.js
+++ src/components/homepage/components/contact.js
@@ -3,13 +3,13 @@ import profile from "../../icons/profile-plus.png"
 
 function Contact() {
   return (
-    <div className="flex flex-col space-y-2 bg-amber px-8 py-24">
-      <div className="flex flex-col space-y-12 sm:w-1/2 self-center">
+    <div className="flex flex-col gap-2 bg-amber px-8 py-16 sm:py-24">
+      <div className="flex flex-col gap-12 sm:gap-20 sm:w-1/2 self-center">
         <p className="generic-subheading">Contact Us</p>
         <div className="flex flex-col sm:flex-row sm:gap-28">
-          <div className="flex flex-col space-y-12">
+          <div className="flex flex-col gap-12">
             <div className="flex flex-col gap-8">
-              <div className="flex flex-col space-y-4 bg-white border-black border-2 p-5 rounded-3xl sm:w-[33rem] h-[140px]">
+              <div className="flex flex-col gap-4 bg-white border-black border-2 p-5 rounded-3xl sm:w-[33rem] h-[140px]">
                 <div className="flex justify-between">
                   <div className="flex space-x-2 items-center">
                     <img src={dot} className="w-8"></img>
@@ -20,7 +20,7 @@ function Contact() {
                 <p className="card-body text-body">I want to help Aid Humanity and receive future appeals</p>
               </div>
 
-              <div className="flex flex-col space-y-4 border-gray border-opacity-25 border-2 p-5 rounded-3xl sm:w-[33rem] h-[140px]">
+              <div className="flex flex-col gap-4 border-gray border-opacity-25 border-2 p-5 rounded-3xl sm:w-[33rem] h-[140px]">
                 <div className="flex justify-between">
                   <div className="flex space-x-2 items-center">
                     <img src={dot} className="w-8"></img>
