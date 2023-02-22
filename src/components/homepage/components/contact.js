be6c0210ed9fa6bcd6c27ada5d538d0e4852c44c
muhammadfuaad <fuaad29@gmail.com>
2023-02-22 17:31:30 +0500

    code

--- src/components/homepage/components/contact.js
+++ src/components/homepage/components/contact.js
@@ -5,39 +5,39 @@ function Contact() {
   return (
     <div className="flex flex-col space-y-2 bg-amber px-8 py-24">
       <div className="flex flex-col space-y-12 sm:w-1/2 self-center">
-        <p className="text-black text-[30px] font-bold tracking-[-0.75px] leading-[34px]">Contact Us</p>
-        <div className="flex flex-col sm:flex-row sm:space-x-12">
+        <p className="generic-subheading">Contact Us</p>
+        <div className="flex flex-col sm:flex-row sm:gap-28">
           <div className="flex flex-col space-y-12">
             <div className="flex flex-col gap-8">
-              <div className="flex flex-col space-y-4 bg-white border-black border-2 p-5 rounded-xl sm:w-[33rem] h-[140px]">
+              <div className="flex flex-col space-y-4 bg-white border-black border-2 p-5 rounded-3xl sm:w-[33rem] h-[140px]">
                 <div className="flex justify-between">
                   <div className="flex space-x-2 items-center">
                     <img src={dot} className="w-8"></img>
-                    <p className="text-black text-[1.8rem] font-bold tracking-[-0.45px] leading-[24px]">Register</p>
+                    <p className="card-heading">Register</p>
                   </div>
                   <img src={profile} className="w-12"></img>
                 </div>
-                <p className="text-body text-[1.6rem] font-normal tracking-[-0.4px] leading-[24px]">I want to help Aid Humanity and receive future appeals</p>
+                <p className="card-body text-body">I want to help Aid Humanity and receive future appeals</p>
               </div>
 
-              <div className="flex flex-col space-y-4 border-gray border-opacity-25 border-2 p-5 rounded-xl sm:w-[33rem] h-[140px]">
+              <div className="flex flex-col space-y-4 border-gray border-opacity-25 border-2 p-5 rounded-3xl sm:w-[33rem] h-[140px]">
                 <div className="flex justify-between">
                   <div className="flex space-x-2 items-center">
                     <img src={dot} className="w-8"></img>
-                    <p className="text-black text-[1.8rem] font-bold tracking-[-0.45px] leading-[24px]">Fundraiser</p>
+                    <p className="card-heading">Fundraiser</p>
                   </div>
                   <img src={profile} className="w-12"></img>
                 </div>
-                <p className="text-body text-[1.6rem] font-normal tracking-[-0.4px] leading-[24px]">Become a fundraiser and kick start your own Charity appeal</p>
+                <p className="card-body text-body">Become a fundraiser and kick start your own Charity appeal</p>
               </div>
             </div>
           </div>
 
-          <div className="flex flex-col gap-10 mt-10 sm:mt-0">
+          <div className="w-full flex flex-col gap-10 mt-10 sm:mt-0">
             <div className="flex flex-col gap-8">
-              <input className="focus:outline-none placeholder-black text-[13px] font-normal tracking-[0px] leading-[15px] bg-amber border-2 border-opacity-50 border-black rounded-lg h-[50px] p-2" placeholder="Name and Surname*"></input>
-              <input className="focus:outline-none placeholder-black text-[13px] font-normal tracking-[0px] leading-[15px] bg-amber border-2 border-opacity-50 border-black rounded-lg h-[50px] p-2" placeholder="Email*"></input>
-              <textarea rows="6" className="focus:outline-none placeholder:text-start placeholder-black text-[13px] font-normal tracking-[0px] leading-[15px] bg-amber border-2 border-opacity-50 border-black rounded-lg h-[98px] p-2" placeholder="Please provide as much detailed information as possible. Thank you *"></textarea>
+              <input className="focus:outline-none placeholder-black text-[13px] font-normal tracking-[0px] leading-[15px] bg-amber border-2 border-opacity-50 border-black rounded-xl h-[50px] p-2" placeholder="Name and Surname*"></input>
+              <input className="focus:outline-none placeholder-black text-[13px] font-normal tracking-[0px] leading-[15px] bg-amber border-2 border-opacity-50 border-black rounded-xl h-[50px] p-2" placeholder="Email*"></input>
+              <textarea rows="6" className="focus:outline-none placeholder:text-start placeholder-black text-[13px] font-normal tracking-[0px] leading-[15px] bg-amber border-2 border-opacity-50 border-black rounded-xl h-[98px] p-2" placeholder="Please provide as much detailed information as possible. Thank you *"></textarea>
             </div>
             <button className="self-start w-full sm:w-fit px-20 h-20 py-6 uppercase text-[1.4rem] font-semibold text-black bg-green rounded-xl">
               Submit Message
