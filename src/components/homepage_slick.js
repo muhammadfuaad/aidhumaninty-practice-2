dd07ecc71dc7284ba0331e5a8f198c80f50051af
muhammadfuaad <fuaad29@gmail.com>
2023-03-09 10:07:53 +0500

    code

--- src/components/homepage_slick.js
+++ src/components/homepage_slick.js
@@ -3,6 +3,7 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Homepage_hero_slider from "./homepage/components/homepage_hero_slider";
+import Hero_card from "./homepage/components/hero_card";
 
 function Homepage_slick() {
   const settings = {
@@ -30,10 +31,14 @@ function Homepage_slick() {
   };
   return (
     <div className="slick homepage relative">
+      <div className="hidden sm:flex gap-10 absolute bottom-28 right-40 z-10">
+        <Hero_card title="Pakistan Floods"/>
+        <Hero_card title="Support an orphan child"/>
+      </div>
       <Slider {...settings}>
         <div>
           <Homepage_hero_slider
-          title= "Build a water well"
+          title= <p>Build<br></br> a Water Well</p>
           content= "1 in 3 people around the world do not have access to clean drinking water. Women and children often walk for miles each day to collect water to drink, denying them the opportunity to go to school, to work and to thrive."
           category= "Water for all"/>
         </div>
